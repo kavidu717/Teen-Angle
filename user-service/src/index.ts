9394c15dc0dec49287ca2e6542f0152e933e4583
kavidu717 <dushmanthakavidu143@gmail.com>
2026-08-28 20:51:27 +0530

    feat:connect the mongodb database to the user service

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -1,2 +1,32 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db";
 
-console.log("hello")
+
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('User Service is Running!');
+});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`User Service is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start the server:", error);
+  }
+};
+
+startServer();
---
+++ user-service/src/utils/db.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async (): Promise<void> => {
+  try {
+    const mongoURI = process.env.MONGO_URI as string;
+    if (!mongoURI) {
+      throw new Error("MONGO_URI is not defined in .env file");
+    }
+
+    const conn = await mongoose.connect(mongoURI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error}`);
+    process.exit(1); 
+  }
+};
+
+export default connectDB;
