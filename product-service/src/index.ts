8a36545e1988e3ce34d58aa31aace5a4fcc8825f
kavidu717 <dushmanthakavidu143@gmail.com>
2026-08-30 09:18:08 +0530

    feat:connect the database to the project

--- product-service/src/index.ts
+++ product-service/src/index.ts
@@ -1,1 +1,31 @@
-console.log("Product Service is running...");
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './utils/db';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Product Service is Running!');
+});
+
+const startServer = async () => {
+    try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`User Service is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start the server:", error);
+  }
+
+
+}
+startServer();
